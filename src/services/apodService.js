139b029a3eb8f1ed4f856ccdb788fffdbbfee3d1
RaihanStark <raihanyudosaputra30@gmail.com>
2022-01-07 13:43:47 +0700

    add conditional api key

--- src/services/apodService.js
+++ src/services/apodService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey = "DEMO_KEY";
+const apiKey = process.env.REACT_APP_API_KEY || "DEMO_KEY";
 const URL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
 const getApod = () => {
